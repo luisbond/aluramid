5e46f607e7ce1a776865d11a7e532e0f8b8d1c4b
luis <106165465+luisbond@users.noreply.github.com>
2023-09-22 08:16:45 -0300

    sons

--- main.js
+++ main.js
@@ -1,4 +1,40 @@
-alert('oi')
+alert('palmeiras timão')
 
-document.querySelector('.tecla_pom')
-document.querySelector('#som_tecla_pom').play();
+function tocaSomPom(){document.querySelector('.tecla_pom')
+}
+document.querySelector('.tecla_pom').onclick=tocaSomPom;
+
+function tocaSomClap(){document.querySelector('.tecla_clap')
+}
+document.querySelector('.tecla_clap').onclick=tocaSomClap;
+
+function tocaSomTim(){document.querySelector('.tecla_Tim')
+}
+document.querySelector('.tecla_tim').onclick=tocaSomTim;
+
+function tocaSomPuff(){document.querySelector('.tecla_puff')
+}
+document.querySelector('.tecla_puff').onclick=tocaSomPuff;
+
+function tocaSomSplash(){document.querySelector('.tecla_splash')
+}
+document.querySelector('.tecla_splash').onclick=tocaSomSplash;
+
+function tocaSomToim(){document.querySelector('.tecla_toim')
+}
+document.querySelector('.tecla_toim').onclick=tocaSomToim;
+
+function tocaSomPsh(){document.querySelector('.tecla_psh')
+}
+document.querySelector('.tecla_psh').onclick=tocaSomPsh;
+
+function tocaSomTic(){document.querySelector('.tecla_tic')
+}
+document.querySelector('.tecla_tic').onclick=tocaSomTic;
+
+function tocaSomTom(){document.querySelector('.tecla_Tom')
+}
+document.querySelector('.tecla_tom').onclick=tocaSomTom;
+
+
+
